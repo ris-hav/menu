b6ef645eae2f63d9787b49d752f419e671882380
Rishav Saha <rishavsaha@192.168.1.10>
2024-07-12 16:17:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { Menu } from "antd";
+import menuData from "./data/menuData.js";
+
+function buildMenuTree(data) {
+  let menuTree = [];
+  let menuObj = {};
+
+  data.forEach((menuItem) => {
+    menuObj[menuItem.menuId] = { ...menuItem, children: [] };
+  });
+
+  data.forEach((menuItem) => {
+    const { menuId, parentId } = menuItem;
+    if (parentId !== 0) {
+      const parent = menuObj[parentId];
+      if (parent) {
+        parent.children.push(menuObj[menuId]);
+      }
+    } else {
+      menuTree.push(menuObj[menuItem.menuId]);
+    }
+  });
+
+  return menuTree;
+}
+
+const data = buildMenuTree(menuData);
+console.log(data);
 
 function App() {
+  const renderMenuItems = (items) => {
+    return items.map((item) => {
+      if (item.children?.length > 0) {
+        return (
+          <Menu.SubMenu key={item.menuId} title={item.item}>
+            {renderMenuItems(item.children)}
+          </Menu.SubMenu>
+        );
+      } else {
+        return <Menu.Item key={item.menuId}>{item.item}</Menu.Item>;
+      }
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Menu
+      mode="inline"
+      style={{
+        width: 256,
+      }}
+      // items={data}
+    >
+      {renderMenuItems(data)}
+    </Menu>
   );
 }
 
